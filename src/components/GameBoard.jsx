1ff691de540b35a12f29b39014711e67a16ae974
Jaspreet Huda <roshan7677@gmail.com>
2025-11-06 03:47:49 +0530

    Lifting State up

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,16 +6,17 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function gameBoardBtnHandler(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
       // Create a deep copy of the previous game board
       const updatedBoard = prevGameBoard.map((innerArray) => [...innerArray]);
-      updatedBoard[rowIndex][colIndex] = "X";
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
+    onSelectSquare();
   }
 
   return (
