c51fc5862e48882a365c64fac1ad0e02c6a1828e
Jaspreet Huda <roshan7677@gmail.com>
2025-11-06 02:36:21 +0530

    Added Player and GameBoard components as well to the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx';
 
 function App() {
-  
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+      <ol id="players" >
+      <Player initialName="Player 1" symbol="X"/>
+      <Player initialName="Player 2" symbol="0"/>
+      </ol>
+      <GameBoard />
+      </div>
+      LOG
+    </main>
   )
 }
 
---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+
+
+export default function Player({initialName, symbol}) {
+    
+    const [ playerName, setPlayerName ] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false);
+    
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+    
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    function handleChange(e) {
+        setPlayerName(e.target.value);
+    }
+
+    if(isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange= {handleChange} />
+    }
+
+    return (
+    <li>
+    <span className="player">
+    {editablePlayerName}
+    <span className="player-symbol">{symbol}</span>
+    </span> 
+    <button onClick={handleEditClick}>{(!isEditing ? 'Edit' : 'Save')}</button>
+    </li>
+    ) 
+}
